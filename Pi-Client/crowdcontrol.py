792b9c1e2d0c427f887d9f3a64d1dbe7241b0472
Robert Ozimek <robertozimek90@gmail.com>
2015-11-26 23:14:29 -0500

    set static camera setting, and started video capture

--- Pi-Client/crowdcontrol.py
+++ Pi-Client/crowdcontrol.py
@@ -7,7 +7,19 @@ import imutils
 
 # initalize camera
 camera = PiCamera()
-
 camera.resolution = (640, 480)
 camera.framerate = 24
 
+# setup static exposure/shutter/white balance/gain settings
+time.sleep(2)
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+
+# capture video
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# camera warmup
+time.sleep(0.1)
