c801bf04b323b3cd32f4212ac430dc5add453f15
mloconte <michael.j.loconte@gmail.com>
2015-12-21 22:22:24 -0500

    Android App

---
+++ Android/app/src/androidTest/java/crowdcontrolandroid/crowdcontrol/ApplicationTest.java
@@ -0,0 +1,11 @@
+package crowdcontrolandroid.crowdcontrol;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
