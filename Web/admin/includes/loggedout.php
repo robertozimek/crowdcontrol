879e3c9b5009b126e0de704616f9b7276983c8a1
t3nzy <Tenzin.Lama28@myhunter.cuny.edu>
2015-12-22 23:48:35 -0500

    Create loggedout.php

---
+++ Web/admin/includes/loggedout.php
@@ -0,0 +1,8 @@
+
+ <div id="main_body">
+            	<div id="content"> You are on the administration side.
+                    <div id="login">
+                        <p> You have successfully logged out. </p>
+                    </div>
+                </div>
+ </div>
