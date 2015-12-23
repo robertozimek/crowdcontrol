70747d73935f974ea568ba5570c93469de839ec3
t3nzy <Tenzin.Lama28@myhunter.cuny.edu>
2015-12-22 23:46:50 -0500

    Create addsuccess.php

---
+++ Web/admin/includes/addsuccess.php
@@ -0,0 +1,9 @@
+<div id="main_body">
+	<div id="content">  You are on the administration side. 
+    	</br></br>
+        <div id="login">
+        	<p><?php echo $user; ?>, your edit was successful. Browse the menu on top for more options.
+            </p>
+        </div>
+	</div>
+</div>
