1123b140c2ac175fdad7115e00d73118df0bc6e2
t3nzy <Tenzin.Lama28@myhunter.cuny.edu>
2015-12-22 23:35:57 -0500

    Create aboutus.php

---
+++ Web/includes/aboutus.php
@@ -0,0 +1,7 @@
+ <div id="main_body">
+    	<div id="content">
+        	<h1> About Us </h1>
+            <p> we are a group of Students from Hunter College doing our final capstone project.</p>
+    			
+    	</div>
+    </div>
