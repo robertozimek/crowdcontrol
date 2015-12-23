64ba7ca8edb702703797a8b3c8889fb35efb9c9d
t3nzy <Tenzin.Lama28@myhunter.cuny.edu>
2015-12-22 23:49:45 -0500

    Create main_body.php

---
+++ Web/admin/includes/main_body.php
@@ -0,0 +1,16 @@
+ 
+ 
+ <div id="main_body">
+            	<div id="content"> You are on the administration side.
+                    <div id="login">
+                        <form action="login.php" method="post">
+                          Admin Log In: 
+                          </br></br>
+                            Username: <input name="username" type="text" value="Your email address" onfocus="if(this.value == 'Your email address') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'Your email address'; }" size="30" maxlength="50" />
+                                    </br></br>
+                            Password: <input name="password" type="password" value="" size="30" maxlength="50"  /> </br></br>
+                            <input type="submit" value="Log In" size="30"> </br>
+                      </form>
+                    </div>
+                </div>
+ </div>
