8eb70d3572a601b29a2093b12c258583fd9d572d
adriantam18 <adrian.tamayo39@myhunter.cuny.edu>
2015-12-22 23:34:36 -0500

    Added comments

--- Web/includes/database.php
+++ Web/includes/database.php
@@ -1,8 +1,9 @@
 <?php
-    $servername = "SERVER_NAME";
-    $username = "USER_NAME";
-    $password = "PASSWORD";
-    $database = "DB_NAME";
+    //Better to not hardcode these information and instead use getenv
+    $servername = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+    $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $database = getenv('OPENSHIFT_APP_NAME');
 
     // Create connection
     $db = new mysqli($servername, $username, $password, $database);
